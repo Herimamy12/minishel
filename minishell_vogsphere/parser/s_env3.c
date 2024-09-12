6c62c7f93cfd6a7e8b97986729e558050ee50553
niriantsoa03 <haritianark@gmail.com>
2024-09-12 19:29:09 +0300

    fixed the memory leaks caused by the export function if the syntax is export env_value update the export function, now it handle fully the case wether export env_var or export env_var= or export env_var= or exoirt env_var=value with no leaks

--- minishell_vogsphere/parser/s_env3.c
+++ minishell_vogsphere/parser/s_env3.c
@@ -13,69 +13,34 @@
 #include "s_env.h"
 #include "utility.h"
 
-t_list	*env_2_lst(t_env *env)
+static int	does_env_var_exist(char *var_name, t_env *env)
 {
-	t_list	*lst;
-	char	*tmp0;
-	char	*tmp1;
-
-	lst = NULL;
 	while (env)
 	{
-		tmp0 = ft_strjoin(env->name, "=");
-		tmp1 = ft_strjoin(tmp0, env->value);
-		free(tmp0);
-		ft_lstadd_back(&lst, ft_lstnew(tmp1));
+		if (!ft_strcmp(var_name, env->value))
+			return (1);
 		env = env->next;
 	}
-	return (lst);
+	return (0);
 }
 
-t_env	*dup_env_lst(t_env *env_lst)
+int	set_env_var(char *var_name, char *new_value, t_env *env, int index)
 {
-	t_env	*env;
-
-	env = NULL;
-	while (env_lst)
+	if (index == -1 && does_env_var_exist(var_name, env))
 	{
-		insert_2_env_lst(ft_strdup(env_lst->name),
-			ft_strdup(env_lst->value), 3, &env);
-		env_lst = env_lst->next;
+		free(new_value);
+		return (1);
 	}
-	return (env);
-}
-
-void	swap_env_lst_items(t_env *env1, t_env *env2)
-{
-	char	*tmp_name;
-	char	*tmp_value;
-	int		tmp_index;
-
-	tmp_name = env1->name;
-	tmp_value = env1->value;
-	tmp_index = env1->index;
-	env1->name = env2->name;
-	env1->value = env2->value;
-	env1->index = env2->index;
-	env2->name = tmp_name;
-	env2->value = tmp_value;
-	env2->index = tmp_index;
-}
-
-void	sort_env_lst(t_env *env)
-{
-	t_env	*tmp;
-
 	while (env)
 	{
-		tmp = env->next;
-		while (tmp)
+		if (!ft_strcmp(var_name, env->name))
 		{
-			if (ft_strcmp(env->name, tmp->name) > 0)
-				swap_env_lst_items(env, tmp);
-			else
-				tmp = tmp->next;
+			free(env->value);
+			env->value = new_value;
+			env->index = index;
+			return (1);
 		}
 		env = env->next;
 	}
+	return (0);
 }
