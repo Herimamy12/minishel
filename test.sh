b9dc6e948b5bd0eb761088600c83f6ec84b61f32
nirirako <nirirako@student.42antananarivo.mg>
2024-09-25 09:31:45 +0300

    edge case that should be tested before any evaluation

---
+++ test.sh
@@ -0,0 +1,118 @@
+echo $.$.
+
+echo "$.$."
+
+echo '"$.$."''
+
+echo "'$.$.'"
+
+echo $?+$?
+
+echo $? + $?
+
+echo "$?+$?"
+
+echo "$? + $?"
+
+echo '$?+$?'
+
+echo '$? + $?'
+
+echo $$
+
+echo "$$"
+
+echo '$$'
+
+echo $$?
+
+echo "$$?"
+
+echo '$$?'
+
+echo $$??
+
+echo "$$??"
+
+echo '$$??'
+
+echo $123abc
+
+echo $abc123abc
+
+echo $a1bc
+
+echo $PWD?
+
+echo $PWD??
+
+echo ?$PWD??
+
+echo "$PWD?"
+
+echo "$PWD??"
+
+echo "?$PWD??"
+
+echo "??$PWD."
+
+echo $PWD.
+
+echo .$PWD.
+
+echo "$PWD."
+
+echo ".$PWD."
+
+echo "..$PWD.."
+
+echo $PWD$.
+
+echo "$PWD$."
+
+echo "$..$PWD$.."
+
+echo $PWD . $PWD
+
+echo "$PWD . $PWD"
+
+echo ".$PWD		.		$PWD."
+
+<<eof cat
+$?
+$??
+$.$<$PWD
+"$?"
+"$??"
+"$<$."
+"$."
+".$#$PWD"
+"$???.$'PWD'"'$HOME'$EMPTY
+'EOF'$PWD'$HOME'$?.$?'
+eof
+
+<<eof' cat
+$?
+$??
+$.$<$PWD
+"$?"
+"$??"
+"$<$."
+"$."
+".$#$PWD"
+"$???.$'PWD'"'$HOME'$EMPTY
+'										'
+eof
+
+
+# ambiguous redirect
+
+< $TSISY cat
+
+export var="abc def"
+<var cat > out
+
+< minishell.h cat -e > $PATH
+
+< minishell.h cat -e > $TSISY
+
