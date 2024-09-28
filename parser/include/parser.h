3134867d63a5fdf64641e3116d416015b4d1aad0
nirirako <nirirako@student.42antananarivo.mg>
2024-09-28 14:20:43 +0300

    rectified norminette of parser.h

--- parser/include/parser.h
+++ parser/include/parser.h
@@ -21,7 +21,7 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-t_token		*token_parser(char *cmd, t_shell *sh);
+t_token		*token_parser(char *cmd, t_shell *sh, int *exit_code);
 t_token		*prompt_user(t_shell *sh);
 t_command	*parse_command(t_token *token);
 #endif
